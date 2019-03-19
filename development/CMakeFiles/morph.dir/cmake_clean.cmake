8f924d1916293b2cbcb9cdc5b9ef31de8910f6bc
Riza Sal <rza108@gmail.com>
2019-03-19 15:10:24 +0530

    added auto hsv

---
+++ development/CMakeFiles/morph.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/morph.dir/morph.cpp.o"
+  "morph.pdb"
+  "morph"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/morph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
